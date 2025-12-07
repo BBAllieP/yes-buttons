d8974dd860670f339179d4fad5f66a17e33729c4
Allie Piemont <brabrah@protonmail.com>
2025-12-07 11:10:00 -0500

    fix unused

--- src/components/soundButton/button.jsx
+++ src/components/soundButton/button.jsx
@@ -1,20 +1,11 @@
 import React, { useState } from "react";
 import "./button.css";
 export default function SoundButton({ onClick, text }) {
-	const [isPressed, setIsPressed] = useState(false);
-	const [isHovered, setIsHovered] = useState(false);
-
 	return (
 		<div className='button-container'>
 			<div className='button-case'>
 				<div className='button-inside'>
-					<button
-						className={`yes-button`}
-						onClick={onClick}
-						onMouseDown={() => setIsPressed(true)}
-						onMouseUp={() => setIsPressed(false)}
-						onMouseEnter={() => setIsHovered(false)}
-						onMouseLeave={() => setIsHovered(false)}>
+					<button className={`yes-button`} onClick={onClick}>
 						{text}
 					</button>
 				</div>
