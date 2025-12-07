3485e71a9739b3195613237458f1813ba8b8258c
Allie Piemont <brabrah@protonmail.com>
2025-12-07 11:13:55 -0500

    remove tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,4 @@ import App from "./App";
 
 test("renders learn react link", () => {
 	render(<App />);
-	const linkElement = screen.getByText(/YES/i);
-	expect(linkElement).toBeInTheDocument();
 });
